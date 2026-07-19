209883572372e6c31c39ec0607771d322373de07
Kevin McHenry <konakevin@gmail.com>
2026-07-19 02:24:13 -0600

    Release v1.0.8
    Marketing version 1.0.7 → 1.0.8.

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ module.exports = {
   expo: {
     name: 'DreamBot',
     slug: 'dreambot',
-    version: '1.0.7',
+    version: '1.0.8',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'dreambot',
